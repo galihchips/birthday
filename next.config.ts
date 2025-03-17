4e3f491d72f229aceb6ecc50bff6751adf32c333
Galih Wibisono <galih.wibisono@ifg-life.id>
2025-03-17 14:19:00 +0700

    add vercel json

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  basePath: "/birthday", // Change to your repo name
-  images: {
-    unoptimized: true, // Required for GitHub Pages
-  },
 };
 
 export default nextConfig;
