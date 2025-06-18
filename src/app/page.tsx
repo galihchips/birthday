dec8dc2efa7892d917acb39136f9a1ce806e156c
Galih Wibisono <galih.wibisono@ifg-life.id>
2025-06-18 16:12:39 +0700

    revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Book from "./component/Book";
 
 export default function Birthday() {
-  const [page, setPage] = useState<"cover" | "photos" | "wishes" | "finish">("finish");
+  const [page, setPage] = useState<"cover" | "photos" | "wishes" | "finish">("cover");
 
   return <Book page={page} setPage={setPage} />;
 }
